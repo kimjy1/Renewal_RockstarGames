2ae341c85fb1b02a52ef6bc1631b605386cec0b0
kimjy1 <uiop9594@gmail.com>
2021-11-29 17:48:54 +0900

    social complete
    aside edit

--- script/main.js
+++ script/main.js
@@ -1,3 +1,8 @@
 document.querySelector('.home').onclick = function () {
   location.reload();
 }
+
+function test() {
+  const MovingBox = document.querySelector('.click1');
+  MovingBox.innerHTML = "테스트";
+}
