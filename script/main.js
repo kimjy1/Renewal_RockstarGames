9c010949cefeae57b1ccc917f50792b7147094d1
kimjy1 <uiop9594@gmail.com>
2021-11-29 19:04:45 +0900

    edit
    edit aside

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ document.querySelector('.home').onclick = function () {
   location.reload();
 }
 
-function test() {
-  const MovingBox = document.querySelector('.click1');
-  MovingBox.innerHTML = "테스트";
+const test1 = document.getElementsByClassName('click1');
+for (var i = 0; i < test1.length; i++) {
+  test1[i].style.transform = "scale(1.1)";
 }
