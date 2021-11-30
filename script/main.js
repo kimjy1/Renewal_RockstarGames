87fa27b619cd6f8d96e7fd8f65efc34ac453810e
kimjy1 <uiop9594@gmail.com>
2021-11-30 17:17:46 +0900

    aside edit
    script add

--- script/main.js
+++ script/main.js
@@ -2,7 +2,44 @@ document.querySelector('.home').onclick = function () {
   location.reload();
 }
 
-const test1 = document.getElementsByClassName('click1');
-for (var i = 0; i < test1.length; i++) {
-  test1[i].style.transform = "scale(1.1)";
+window.onload = function () {
+  clickevent1();
+}
+
+const container = document.getElementById('image3dbones');
+
+const clicktest1 = document.getElementById('click1');
+clicktest1.addEventListener("click", clickevent1);
+function clickevent1() {
+  container.style.alignContent = "flex-end";
+  clicktest1.style.transform = "scale(1.2)";
+  clicktest1.style.transition = "all .5s ease-in-out";
+  clicktest2.style.transform = "scale(0.9)";
+  clicktest2.style.transition = "all .5s ease-in-out";
+  clicktest3.style.transform = "scale(0.9)";
+  clicktest3.style.transition = "all .55s ease-in-out";
+}
+
+const clicktest2 = document.getElementById('click2');
+clicktest2.addEventListener("click", clickevent2);
+function clickevent2() {
+  container.style.alignContent = "center";
+  clicktest1.style.transform = "scale(0.9)";
+  clicktest1.style.transition = "all .5s ease-in-out";
+  clicktest2.style.transform = "scale(1.2)";
+  clicktest2.style.transition = "all .5s ease-in-out";
+  clicktest3.style.transform = "scale(0.9)";
+  clicktest3.style.transition = "all .5s ease-in-out";
+}
+
+const clicktest3 = document.getElementById('click3');
+clicktest3.addEventListener("click", clickevent3);
+function clickevent3() {
+  container.style.alignContent = "flex-start";
+  clicktest1.style.transform = "scale(0.9)";
+  clicktest1.style.transition = "all .5s ease-in-out";
+  clicktest2.style.transform = "scale(0.9)";
+  clicktest2.style.transition = "all .5s ease-in-out";
+  clicktest3.style.transform = "scale(1.2)";
+  clicktest3.style.transition = "all .5s ease-in-out";
 }
