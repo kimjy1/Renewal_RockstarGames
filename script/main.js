5b0bc09d781628c932b01a2ccb2edb2a1f88d8db
kimjy1 <uiop9594@gmail.com>
2021-12-08 17:53:28 +0900

    mediaquery mobile complete
    mobile success

--- script/main.js
+++ script/main.js
@@ -12,16 +12,12 @@ $(function () {
 $(window).resize(function () {
   if (window.innerWidth >= 768) {
     $("#headernav").show();
-  };
+  }
 });
 
 // if (window.matchMedia("(max-width: 380px)").matches) { 
 //   clicktest1.removeEventListener(clickevent1);
 // }
-
-if (window.matchMedia("(max-width: 380px)").matches) {
-  document.getElementsByTagName("p")[6].innerText = "";
-}
 
 // 게임소개 컨텐츠 중 가운데 스크립트 시작
 const container = document.getElementById('image3dbones');
