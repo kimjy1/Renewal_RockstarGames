15045bd92e0bedc16aa986fce72d334bd0f3305b
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2021-12-11 23:51:54 +0900

    mediaquery edit
    editing

--- script/main.js
+++ script/main.js
@@ -5,13 +5,19 @@ $(window).resize(function () {
   }
 });
 
+// 모바일 메뉴 효과 (제이쿼리)
 $(function () {
   $(".mobile").click(function () {
     $("#headernav").slideToggle("slow");
   });
 });
 
-// 게임소개 컨텐츠 중 가운데 스크립트 시작
+// 로고 클릭 시 웹 브라우저 새로고침
+document.querySelector('.home').onclick = function () {
+  location.reload();
+}
+
+// 게임소개 컨텐츠 상수 선언
 const container = document.getElementById('image3dbones');
 const clicktest1 = document.getElementById('click1');
 const clicktest2 = document.getElementById('click2');
@@ -23,21 +29,11 @@ const imageChange1 = document.getElementById('img1');
 const imageChange2 = document.getElementById('img2');
 const imageChange3 = document.getElementById('img3');
 
-// 웹 브라우저 시작 시 클릭 이벤트 함수 실행
-window.onload = function () {
-  clickevent1();
-
-  // 로고 클릭 시 웹 브라우저 새로고침
-  document.querySelector('.home').onclick = function () {
-    location.reload();
-  }
-}
-
+// 반응형 - 게임소개 컨텐츠 스크립트(PC)
 if (window.matchMedia("(min-width: 1024px) and (max-width:1920px)").matches) {
   clicktest1.addEventListener("click", clickevent1);
   function clickevent1() {
     container.style.transform = "translateY(100px)";
-    // container.style.transform = "translateX(100px)";
     container.style.transition = "all .9s ease";
     clicktest1.style.transform = "scale(1.2)";
     clicktest1.style.transition = "all .5s ease-in-out";
@@ -56,7 +52,6 @@ if (window.matchMedia("(min-width: 1024px) and (max-width:1920px)").matches) {
   clicktest2.addEventListener("click", clickevent2);
   function clickevent2() {
     container.style.transform = "translateY(0px)";
-    // container.style.transform = "translateX(0px)";
     container.style.transition = "all .9s ease";
     clicktest1.style.transform = "scale(0.8)";
     clicktest1.style.transition = "all .5s ease-in-out";
@@ -75,7 +70,6 @@ if (window.matchMedia("(min-width: 1024px) and (max-width:1920px)").matches) {
   clicktest3.addEventListener("click", clickevent3);
   function clickevent3() {
     container.style.transform = "translateY(-100px)";
-    // container.style.transform = "translateX(-100px)";
     container.style.transition = "all .9s ease";
     clicktest1.style.transform = "scale(0.8)";
     clicktest1.style.transition = "all .5s ease-in-out";
@@ -91,19 +85,78 @@ if (window.matchMedia("(min-width: 1024px) and (max-width:1920px)").matches) {
     textbox3.style.display = "block";
   }
 
+  window.onload = function () {
+    clickevent1();
+  }
 
 } else {
-  removeEventListener(clickevent1, clickevent2, clickevent3);
+  clicktest1.removeEventListener("click", clickevent1);
+  clicktest2.removeEventListener("click", clickevent2);
+  clicktest3.removeEventListener("click", clickevent3);
 }
 
-if (window.matchMedia("(max-width: 380px)").matches) {
-  imageChange1.style.display = "block";
-  imageChange2.style.display = "none";
-  imageChange3.style.display = "none";
-  textbox1.style.display = "block";
-  textbox2.style.display = "none";
-  textbox3.style.display = "none";
+// 반응형 - 게임소개 컨텐츠 스크립트(모바일)
+if (window.matchMedia("(min-width: 380px)").matches) {
+  clicktest1.addEventListener("click", mobileEvent1);
+  function mobileEvent1() {
+    container.style.transform = "translateX(100px)";
+    container.style.transition = "all .9s ease";
+    clicktest1.style.transform = "scale(1.0)";
+    clicktest1.style.transition = "all .5s ease-in-out";
+    clicktest2.style.transform = "scale(0.8)";
+    clicktest2.style.transition = "all .5s ease-in-out";
+    clicktest3.style.transform = "scale(0.8)";
+    clicktest3.style.transition = "all .5s ease-in-out";
+    imageChange1.style.display = "block";
+    imageChange2.style.display = "none";
+    imageChange3.style.display = "none";
+    textbox1.style.display = "block";
+    textbox2.style.display = "none";
+    textbox3.style.display = "none";
+  }
+
+  clicktest2.addEventListener("click", mobileEvent2);
+  function mobileEvent2() {
+    container.style.transform = "translateX(0px)";
+    container.style.transition = "all .9s ease";
+    clicktest1.style.transform = "scale(0.8)";
+    clicktest1.style.transition = "all .5s ease-in-out";
+    clicktest2.style.transform = "scale(1.0)";
+    clicktest2.style.transition = "all .5s ease-in-out";
+    clicktest3.style.transform = "scale(0.8)";
+    clicktest3.style.transition = "all .5s ease-in-out";
+    imageChange1.style.display = "none";
+    imageChange2.style.display = "block";
+    imageChange3.style.display = "none";
+    textbox1.style.display = "none";
+    textbox2.style.display = "block";
+    textbox3.style.display = "none";
+  }
+
+  clicktest3.addEventListener("click", mobileEvent3);
+  function mobileEvent3() {
+    container.style.transform = "translateX(-100px)";
+    container.style.transition = "all .9s ease";
+    clicktest1.style.transform = "scale(0.8)";
+    clicktest1.style.transition = "all .5s ease-in-out";
+    clicktest2.style.transform = "scale(0.8)";
+    clicktest2.style.transition = "all .5s ease-in-out";
+    clicktest3.style.transform = "scale(1.0)";
+    clicktest3.style.transition = "all .5s ease-in-out";
+    imageChange1.style.display = "none";
+    imageChange2.style.display = "none";
+    imageChange3.style.display = "block";
+    textbox1.style.display = "none";
+    textbox2.style.display = "none";
+    textbox3.style.display = "block";
+  }
+
+  window.onload = function () {
+    mobileEvent1();
+  }
+
 } else {
-
+  clicktest1.removeEventListener("click", mobileEvent1);
+  clicktest2.removeEventListener("click", mobileEvent2);
+  clicktest3.removeEventListener("click", mobileEvent3);
 }
-
