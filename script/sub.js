0acf907560028d93367afb18a1ef4b1f839bc715
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2021-12-23 17:57:26 +0900

    video file upload prepare
    git lfs use prepare

--- script/sub.js
+++ script/sub.js
@@ -3,3 +3,12 @@ $(function () {
     e.preventDefault();
   });
 });
+
+var $wrapper = $('.ComparisonVideo');
+var $clipper = $('.after');
+
+$wrapper.on('mousemove', function (e) {
+  var rect = $wrapper[0].getBoundingClientRect();
+  var position = ((e.pageX - rect.left) / $wrapper[0].offsetWidth) * 100;
+  $clipper.css('width', position + '%');
+});
