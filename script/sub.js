0edf42ca6f47a4d47388a184c29261be1fa4bbed
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2021-12-24 15:59:27 +0900

    sub page coding
    1. video comparison slide complete
    2. pre-purchase section coding

--- script/sub.js
+++ script/sub.js
@@ -4,6 +4,23 @@ $(function () {
   });
 });
 
+// 이미지 클릭시 비디오 및 타이틀 체인지
+$(function () {
+  $(".logoImg1").click(function () {
+    $(".video1, .gta_Title1").show();
+    $(".video2, .video3, .gta_Title2, .gta_Title3").hide();
+  });
+  $(".logoImg2").click(function () {
+    $(".video2, .gta_Title2").show();
+    $(".video1, .video3, .gta_Title1, .gta_Title3").hide();
+  });
+  $(".logoImg3").click(function () {
+    $(".video3, .gta_Title3").show();
+    $(".video1, .video2, .gta_Title1, .gta_Title2").hide();
+  });
+});
+
+// 비디오 비교 슬라이더 
 var $wrapper = $('.ComparisonVideo');
 var $clipper = $('.after');
 
@@ -12,3 +29,4 @@ $wrapper.on('mousemove', function (e) {
   var position = ((e.pageX - rect.left) / $wrapper[0].offsetWidth) * 100;
   $clipper.css('width', position + '%');
 });
+
