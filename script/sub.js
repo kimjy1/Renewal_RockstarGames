9b3dccf571c0477941d67f95ddcc98a414c9ee1e
kimjy1 <93234891+kimjy1@users.noreply.github.com>
2021-12-29 15:48:49 +0900

    comparison slider editing
    mobile : add a touch event

--- script/sub.js
+++ script/sub.js
@@ -38,7 +38,7 @@ $(function () {
 var $wrapper = $('.ComparisonVideo');
 var $clipper = $('.after');
 
-$wrapper.on('mousemove', function (e) {
+$wrapper.on('mousemove touchmove', function (e) {
   var rect = $wrapper[0].getBoundingClientRect();
   var position = ((e.pageX - rect.left) / $wrapper[0].offsetWidth) * 100;
   $clipper.css('width', position + '%');
